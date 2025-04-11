519ccc92ff4a641437d619d7b3b1f3dcf0118472
Vadim Savin <savinvadim1312@gmail.com>
2025-04-11 16:36:26 +0200

    Bottom Tabs

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { FlatList } from 'react-native';
+
+import books from '@/dummyBooks';
+import BookListItem from '@/components/BookListItem';
+
+export default function App() {
+  return (
+    <FlatList
+      data={books}
+      contentContainerClassName='gap-4 p-2'
+      renderItem={({ item }) => <BookListItem book={item} />}
+    />
+  );
+}
