9fa09b6ac43b9ac5b3389746efbfb8379964c50b
Vadim Savin <savinvadim1312@gmail.com>
2025-04-11 15:48:26 +0200

    NativeWind installation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
+import './global.css';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='bg-slate-800 flex-1 items-center justify-center'>
+      <Text className='text-2xl text-gray-100 font-bold'>
+        Hello world from NativeWind
+      </Text>
+
+      <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
